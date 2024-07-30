415aa724dbcba62fa24653f3177ca9cabdb920e1
LehMipo412 <barbetzalel412@gmail.com>
2024-07-30 16:29:06 +0300

    wtf is going on im very sad now unity is trolling

--- UnityOnline/Assets/Scripts/FollowMouse.cs
+++ UnityOnline/Assets/Scripts/FollowMouse.cs
@@ -12,24 +12,38 @@ public class FollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //  transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) );//* Time.deltaTime
-        transform.eulerAngles = Input.mousePosition;
+        //transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X") * Time.deltaTime, 0) );
+        //transform.localEulerAngles = Input.mousePosition;
+        var horizonAngle = transform.localEulerAngles.x;
 
-        if (transform.eulerAngles.x > 180)
+        if (horizonAngle > 180 && horizonAngle <340)
         {
-            transform.eulerAngles = new Vector3(180f, transform.eulerAngles.y, 0);
+            horizonAngle = 340;
+            
         }
-        if (transform.eulerAngles.y > 180)
+        else if(horizonAngle < 180 && horizonAngle > 40)
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180f, 0);
+            horizonAngle = 40;
         }
-        if (transform.eulerAngles.x < 0)
-        {
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-        }
-        if (transform.eulerAngles.y < 0)
-        {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, 0);
-        }
+
+        transform.localEulerAngles = new Vector3(horizonAngle, transform.localEulerAngles.y, 0);
+        
+
+        //if (transform.eulerAngles.x > 180)
+        //{
+        //    transform.eulerAngles = new Vector3(180f, transform.eulerAngles.y, 0);
+        //}
+        //if (transform.eulerAngles.y > 180)
+        //{
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180f, 0);
+        //}
+        //if (transform.eulerAngles.x < 0)
+        //{
+        //    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        //}
+        //if (transform.eulerAngles.y < 0)
+        //{
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, 0);
+        //}
     }
 }
